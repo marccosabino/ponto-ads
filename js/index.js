91a49642558ef446c631cbbfb6a6d60aa508b2f5
marccosabino <marco.sabinosr@gmail.com>
2024-09-05 09:28:17 -0300

    Update - 05/09/2024

--- js/index.js
+++ js/index.js
@@ -1,21 +1,34 @@
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno =  document.getElementById("dia-mes-ano");
 const horaMinSeg =  document.getElementById("hora-min-seg");
+const arrayDayWeek = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"]
 
-
-function diaSemana(){
-    
+function daySemana(){
+    const date = new Date();
+    return arrayDayWeek[date.getDay()]
 }
+
 
 function dataCompleta() {
     const date = new Date();
     return date.getDate() + "/" +  (date.getMonth() + 1) + "/" + date.getFullYear();
 }
 
+
 function horaCompleta() {
     const date = new Date();
     return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
+
+
+function atualizaHora() {
+    horaMinSeg.textContent = horaCompleta();
+}
+
+
+setInterval(atualizaHora, 1000);
+
+
+diaSemana.textContent = daySemana();
 diaMesAno.textContent = dataCompleta();
-horaMinSeg.textContent = horaCompleta();
